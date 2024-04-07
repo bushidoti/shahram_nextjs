7b4b7773649b04fb5b21a2b1676fa5edad7bf533
Sajjad <titanxl79@gmail.com>
2024-04-07 17:19:31 +0330

    remove animate

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/router'
 import AudioPlayer from "react-h5-audio-player";
 import React, {useState} from "react";
 import 'react-h5-audio-player/lib/styles.css';
+import { useAmp } from 'next/amp'
 
 
 interface Type {
@@ -24,7 +25,7 @@ const { Search } = Input;
 export default function Home({ data } : any) {
     const router = useRouter()
     const [playIndex , setPlayIndex] = useState<number>(0)
-    
+
     function handleClickPrevious () {
         setPlayIndex(prevState => prevState === 0 ? data.length - 1 : prevState - 1)
     }
@@ -69,19 +70,22 @@ export default function Home({ data } : any) {
             </Head>
             <Flex vertical>
                 <Flex wrap={"wrap"} gap={40} align={"center"} justify={"center"}>
-                <Flex vertical  align={"center"} justify={"center"}>
+                    <Flex vertical align={"center"} justify={"center"}>
                         <AutoComplete
-                            options={data.map((item : {name : string}) => ({value: item.name  }))}
+                            options={data.map((item: { name: string }) => ({value: item.name}))}
                             className="!mb-2"
-                            filterOption={(inputValue, option : any) =>
-                              option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                            filterOption={(inputValue, option: any) =>
+                                option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
                             }
                         >
-                            <Search className="lg:!w-[40vw] md:!w-[40vw] sm:!w-[40vw] mobile:!w-[80vw]" placeholder="جستجو آهنگ ...." onSearch={onSearch} />
+                            <Search className="lg:!w-[40vw] md:!w-[40vw] sm:!w-[40vw] mobile:!w-[80vw]"
+                                    placeholder="جستجو آهنگ ...." onSearch={onSearch}/>
                         </AutoComplete>
-                           <Carousel dots={false} autoplaySpeed={2000} className={'lg:!w-[40vw] md:w-[40vw] sm:w-[40vw] mobile:w-[80vw] w-[60vw]'} autoplay effect="fade">
-                             {data.map((value: Type , i: number) => (
-                                  <Image
+                        <Carousel dots={false} autoplaySpeed={2000}
+                                  className={'lg:!w-[40vw] md:w-[40vw] sm:w-[40vw] mobile:w-[80vw] w-[60vw]'} autoplay
+                                  effect="fade">
+                            {data.map((value: Type, i: number) => (
+                                <Image
                                     key={`image${i}`}
                                     priority
                                     className={`rounded mobile:h-[50vh] lg:h-[60vh] sm:h-[60vh] md:h-[60vh]`}
@@ -92,70 +96,70 @@ export default function Home({ data } : any) {
                                     src={value.pic}
                                     alt={value.name}
                                 />
-                             ))}
-                         </Carousel>
-                     </Flex>
-                     <Flex vertical  align={"center"} justify={"center"} gap={20}>
-                         <h2><strong>لیست پخش</strong></h2>
+                            ))}
+                        </Carousel>
+                    </Flex>
+                    <Flex vertical align={"center"} justify={"center"} gap={20}>
+                        <h2><strong>لیست پخش</strong></h2>
                         <Card
-                        className='!bg-white !bg-clip-padding !backdrop-filter
+                            className='!bg-white !bg-clip-padding !backdrop-filter
                         !backdrop-blur-sm !bg-opacity-5 shadow-2xl
                          lg:!w-[25vw] md:!w-[40vw] sm:!w-[40vw] mobile:!w-[80vw]'
-                        cover={
-                            <Image
-                                src={data[playIndex].pic}
-                                alt={data[playIndex].pic}
-                                width={0}
-                                height={0}
-                                priority
-                                sizes="200px"
-                                className='w-[500px] h-[350px]'
-                             />
-                        }
-                    >
-                        <div style={{direction: 'ltr'}}>
-                            <AudioPlayer
-                                className='!border-none !bg-white !bg-clip-padding
+                            cover={
+                                <Image
+                                    src={data[playIndex].pic}
+                                    alt={data[playIndex].pic}
+                                    width={0}
+                                    height={0}
+                                    priority
+                                    sizes="200px"
+                                    className='w-[500px] h-[350px]'
+                                />
+                            }
+                        >
+                            <div style={{direction: 'ltr'}}>
+                                <AudioPlayer
+                                    className='!border-none !bg-white !bg-clip-padding
                                  !backdrop-filter !backdrop-blur-sm !bg-opacity-10 '
-                                 onEnded={handleClickNext}
-                                  autoPlayAfterSrcChange={true}
-                                  showSkipControls={true}
-                                  preload={"none"}
-                                  customVolumeControls={[]}
-                                  footer={<h4  className='text-center'>{data[playIndex].description}</h4>}
-                                  header={<h2  className='text-center'>{data[playIndex].name}</h2>}
-                                  showJumpControls={false}
-                                  src={data[playIndex].music}
-                                  onClickPrevious={handleClickPrevious}
-                                  onClickNext={handleClickNext}
-                            />
-                        </div>
-                    </Card>
-                 </Flex>
+                                    onEnded={handleClickNext}
+                                    autoPlayAfterSrcChange={true}
+                                    showSkipControls={true}
+                                    preload={"none"}
+                                    customVolumeControls={[]}
+                                    footer={<h4 className='text-center'>{data[playIndex].description}</h4>}
+                                    header={<h2 className='text-center'>{data[playIndex].name}</h2>}
+                                    showJumpControls={false}
+                                    src={data[playIndex].music}
+                                    onClickPrevious={handleClickPrevious}
+                                    onClickNext={handleClickNext}
+                                />
+                            </div>
+                        </Card>
+                    </Flex>
 
-                     </Flex>
-                     <div className='overflow-auto'>
-                        <h2 className='my-2'>موزیک های اخیر</h2>
-                         <Flex gap={20}>
-                            {data.slice(0).reverse().map((value: Type , i: number) => (
-                                <Link rel='canonical' key={`Link${i}`} href={`/music/${value.name.toLowerCase()}`}>
-                                      <Image
-                                        key={`image${i}`}
-                                        priority
-                                        loading={"eager"}
-                                        className='object-fill rounded w-[250px] h-[250px]'
-                                        width={250}
-                                        height={250}
-                                        sizes={'200px'}
-                                        src={value.pic}
-                                        alt={value.name}
-                                      />
-                                </Link>
-                            ))}
-                         </Flex>
-                     </div>
-        </Flex>
-    </>
+                </Flex>
+                <div className='overflow-auto'>
+                    <h2 className='my-2'>موزیک های اخیر</h2>
+                    <Flex gap={20}>
+                        {data.slice(0).reverse().map((value: Type, i: number) => (
+                            <Link rel='canonical' key={`Link${i}`} href={`/music/${value.name.toLowerCase()}`}>
+                                <Image
+                                    key={`image${i}`}
+                                    priority
+                                    loading={"eager"}
+                                    className='object-fill rounded w-[250px] h-[250px]'
+                                    width={250}
+                                    height={250}
+                                    sizes={'200px'}
+                                    src={value.pic}
+                                    alt={value.name}
+                                />
+                            </Link>
+                        ))}
+                    </Flex>
+                </div>
+            </Flex>
+        </>
     )
 }
 
